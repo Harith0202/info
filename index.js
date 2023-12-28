46572a1e8daee2120f93ff00668562838361d8c5
Harith0202 <harithrosly123@gmail.com>
2023-12-28 15:39:26 +0800

    EDIT

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const options = {
 
 const swaggerSpec = swaggerJsdoc(options);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://harith:Harith123@cluster0.iezu3ww.mongodb.net/?retryWrites=true&w=majority";
@@ -63,8 +63,6 @@ app.use(express.json());
 
 app.post('/register/user', async (req, res) => {
   try {
-    console.log(req.security);
-
     let result = register(
       req.body.username,
       req.body.password,
@@ -72,25 +70,16 @@ app.post('/register/user', async (req, res) => {
       req.body.email,
     );
 
-    // Assuming the register function returns an object with a 'success' property.
     if (result.success) {
       res.status(201).send(result); // 201 Created
     } else {
-      // Here we're changing the response for a registration failure
-      // that normally would send a 400 status.
-      // This should be a temporary fix.
-      res.status(200).send({
-        
-        result: result // You can choose to send back the original result or not.
-      });
+      res.status(400).send(result); // Send the result with a 400 status for registration failure.
     }
   } catch (error) {
     console.error(error);
-    // If you want to suppress the 500 error message, you can change the status code and message here.
-    // Again, not recommended as it hides the error from the user.
-    res.status(200).send({
+    res.status(500).send({
       success: false,
-      message: "The operation completed with warnings, an error occurred.",
+      message: "An error occurred while processing your request.",
       error: error.message // Including the error message is useful for debugging.
     });
   }
